6ed78689837e095c43d3172771ad7bd1d45b9692
Jyothisri-298 <jyothisriguntu298@gmail.com>
2020-11-24 19:10:12 +0530

    Added validation for title,description and target date

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -33,34 +33,55 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-
     upload(req, res, function (err) {
-        if (!err) {
-            var obj = {
-                title: req.body.title,
-                desc: req.body.desc,
-                media: {
-                    data: fs.readFileSync(req.file.path),
-                    contentType: req.file.mimetype
-                },
-                target_dt: req.body.date,
-                status: req.body.status
+        var obj = {
+            title: req.body.title,
+            desc: req.body.desc,
+            target_dt: req.body.date,
+            status: req.body.status
+        }
+        if (req.file) {
+            obj.media = {
+                data: fs.readFileSync(req.file.path),
+                contentType: req.file.mimetype
             }
-            console.log(obj);
-
-           Todo.create(obj, (err, item) => { 
-                if (err) { 
-                    console.log(err); 
-                } 
-                else { 
-                    console.log("SUCESSSSS");//res.redirect('/'); 
-                } 
-            }); 
+        } else {
+            obj.media = null;
         }
-        else{
-
-        }
+        Todo.create(obj, (err, item) => {
+            if (err && err.name == "ValidationError") {
+                handleValidationError(err, req.body);
+                res.render("task/addOrEdit", {
+                    viewTitle: "Insert Task",
+                    task: req.body
+                })
+            } else {
+                res.redirect('task/list');
+            }
+        });
     });
 })
+
+router.get('/list', (req, res) => {
+    res.json('texting')
+})
+
+function handleValidationError(err, body) {
+    for (field in err.errors) {
+        switch (err.errors[field].path) {
+            case 'title':
+                body['titleError'] = err.errors[field].message;
+                break;
+            case 'desc':
+                body['descError'] = err.errors[field].message;
+                break;
+            case 'target_dt':
+                body['dateError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
+}
 
 module.exports = router;
--- models/task.model.js
+++ models/task.model.js
@@ -2,11 +2,25 @@ var mongoose = require( 'mongoose' );
 var Schema   = mongoose.Schema;
  
 var Todo = new Schema({
-    title : String,
-    desc : String,
-    media : { data: Buffer, contentType: String },
-    target_dt : Date,
-    status : String
+    title : {
+        type: String,
+        required: "Title is required !"
+    },
+    desc : {
+        type: String,
+        required: "Description is required !"
+    },
+    media : { 
+        data: Buffer, 
+        contentType: String 
+    },
+    target_dt : {
+        type: Date,
+        required: "Target date is required !"
+    },
+    status : {
+        type: String
+    }
 });
  
 mongoose.model( 'Todo', Todo );
