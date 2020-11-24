622ddeac556d4a69038224ea6e616b68d2fc97b9
Jyothisri-298 <jyothisriguntu298@gmail.com>
2020-11-24 17:47:44 +0530

    Added data storing and File upload functionality

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,10 +1,66 @@
 const express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var multer = require('multer');
+const mongoose = require('mongoose');
+const Todo = mongoose.model("Todo");
 
-router.get('/',(req,res) => {
-    res.render("task/addOrEdit",{
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let dir = "uploads"
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, {
+                recursive: true
+            });
+        }
+        cb(null, dir)
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + file.originalname)
+    }
+});
+
+var upload = multer({
+    storage: storage
+}).single('file');
+
+
+router.get('/', (req, res) => {
+    res.render("task/addOrEdit", {
         viewTitle: "Insert Task"
     })
+})
+
+router.post('/', (req, res) => {
+
+    upload(req, res, function (err) {
+        if (!err) {
+            var obj = {
+                title: req.body.title,
+                desc: req.body.desc,
+                media: {
+                    data: fs.readFileSync(req.file.path),
+                    contentType: req.file.mimetype
+                },
+                target_dt: req.body.date,
+                status: req.body.status
+            }
+            console.log(obj);
+
+           Todo.create(obj, (err, item) => { 
+                if (err) { 
+                    console.log(err); 
+                } 
+                else { 
+                    console.log("SUCESSSSS");//res.redirect('/'); 
+                } 
+            }); 
+        }
+        else{
+
+        }
+    });
 })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const db = require('./models/db');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+const db = require('./models/db');
 const taskController = require('./controllers/taskController');
+
 const app = express();
-
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json());
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
@@ -18,9 +23,11 @@ app.get('/', (req,res) => {
 app.use('/task', taskController);
 
 
+
 app.listen(3000, () => {
     console.log('Server is listening at 3000');
 })
+
 
 
 
