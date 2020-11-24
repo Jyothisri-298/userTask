80a590583f0bd654e7aebe66866829e3546821b1
Jyothisri-298 <jyothisriguntu298@gmail.com>
2020-11-24 12:32:54 +0530

    Created taskController

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var db = require('./models/db');
+const taskController = require('./controllers/taskController');
 var app = express();
 
 
 app.get('/', (req,res) => {
     res.send('Hello');
 });
+
+app.use('/task', taskController);
 
 
 app.listen(3000, () => {
